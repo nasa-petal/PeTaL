94cd0d97a800c7007dce55f4b98cb4c6e2dcd079
Lucas Saldyt <lucassaldyt@gmail.com>
2020-01-15 14:58:51 -0700

    Saves work

--- petal/mining/catalog.py
+++ petal/mining/catalog.py
@@ -2,29 +2,43 @@ from bs4 import BeautifulSoup
 from requests import get
 from selenium import webdriver
 
-import sys, re
+from pprint import pprint
+
+import sys, re, os
 
 year = '2019'
+url = 'https://www.catalogueoflife.org/annual-checklist/{}/browse/tree'.format(year)
+cache_file = 'cached_catalogue_of_life.html'
 
 def main():
-    url = 'https://www.catalogueoflife.org/annual-checklist/{}/browse/tree'.format(year)
-    driver = webdriver.Firefox()
-    driver.get(url)
-    html = driver.page_source
-    #catalog = get('https://www.catalogueoflife.org/annual-checklist/{}/browse/tree'.format(year))
+    # Load raw HTML of COL (ideally a one-off)
+    # Caching = 50x speedup
+    if os.path.isfile(cache_file):
+        # Use cached HTML
+        with open(cache_file, 'r') as infile:
+            html = infile.read()
+    else:
+        driver = webdriver.Firefox()
+        driver.get(url)
+        html = driver.page_source
+        # Cache HTML
+        with open(cache_file, 'w') as outfile:
+            outfile.write(html)
+        driver.quit()
+    # Begin parsing HTML of COL to get Species
     processed = BeautifulSoup(html, 'html.parser')
-    body = processed.body
-    test = body.find(id='right-col').find(id='content').find(id='tree')
-    print(test)
-    #print(dir(test))
-    #print(catalog.text)
-    #print(test.catalog)
-    #print(processed.prettify())
-    #print(dir(processed))
-    #print(processed.findAll(id='tree'))
-    #print(processed.findAll(class='dijitTreeNode'))
-    #print(processed.find_all('div', 'dijitTreeNode'))
-    #print(processed.find_all(id='ACI_dojo_TxTreeNode_0'))
+    body      = processed.body
+
+    phylums = body.find_all(attrs={'class' : 'nodeLabel node-phylum'})
+    print(phylums)
+    return
+    kingdoms = body.find_all(attrs={'class' : 'nodeLabel node-'})
+    for kingdom in kingdoms:
+        parent = kingdom.parent.parent.parent
+        phylums = parent.find_all(attrs={'class' : 'nodeLabel node-phylum'})
+        print(kingdom.text)
+        for phylum in phylums:
+            print('    ' + phylum.text)
 
 if __name__ == '__main__':
     sys.exit(main())
