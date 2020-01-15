8a6e9726ef3630e26a142fa151eef68c2d2a82fb
Lucas Saldyt <lucassaldyt@gmail.com>
2020-01-15 13:04:07 -0700

    Updates PeTaL and expands requirements list. Uses tensorflow 2.1

--- petal/bird_to_biomole.py
+++ petal/bird_to_biomole.py
@@ -9,16 +9,16 @@ import json
 import os
 
 """
-Will create "hierarchy.json and strategies.json" from BIRD data. An error message will be 
-triggered on line 99 due to data inconsistencies from the BIRD data. Due to restriction of 
-time author did not find a solution. For now if parts of BIRD data are cleaned it has worked. 
+Will create "hierarchy.json and strategies.json" from BIRD data. An error message will be
+triggered on line 99 due to data inconsistencies from the BIRD data. Due to restriction of
+time author did not find a solution. For now if parts of BIRD data are cleaned it has worked.
 """
 
 bird_abstracts = ["Articulate", "Actuate", "Allow-DOF", "Analog", "Change", "Collect",
                   "Composite", "Condition", "Contain", "Control", "Convert", "Couple",
-                  "Decrease", "Decrement", "Detect", "Discrete", "Distribute", 
+                  "Decrease", "Decrement", "Detect", "Discrete", "Distribute",
                   "Divide", "Export", "Guide", "Import", "Increase", "Indicate",
-                  "Inhibit", "Join", "Link", "Measure", "Mix", "Particulate", 
+                  "Inhibit", "Join", "Link", "Measure", "Mix", "Particulate",
                   "Prevent", "Process", "Regulate", "Remove", "Rotate", "Secure",
                   "Separate", "Shape", "Stabilize", "Stop", "Store", "Supply", "Support",
                   "Transfer", "Translate", "Transmit", "Transport"]
@@ -37,14 +37,14 @@ for txt_file in bird_abstracts:
             text = reader.read()
     except Exception as e:  # todo: figure out error handling
         print("Error opening file: " + str(e))
-    
+
     abstract_list = [y.strip() for y in text.split(sep=u'\n\n')]  # split abstracts apart
     del abstract_list[-1]  # remove empty entry at the end
     for abstract in abstract_list:
         single_abstract = abstract.split('\n')
         result_list.append(single_abstract)
         current_tertiary_list.append(single_abstract[5])
-    
+
     tertiary_term_dict[txt_file] = current_tertiary_list
 
 
@@ -92,21 +92,21 @@ for secondary1 in python_dict:
     print(secondary1)
     print('///////////////')
     for secondary2 in secondary1["children"]:
-    
+
         for tertiary1 in secondary2["children"]:
             tertiary1["children"] = tertiary_term_dict[str(tertiary1["name"])]
 python_dict = {"name":"ROOT","value":0,"parent":"null","children":python_dict}
 
 
 #######################
-    
+
 test_list = final
-  
-# using naive method to  
-# remove duplicates  
+
+# using naive method to
+# remove duplicates
 res_list = []
-for i in range(len(test_list)): 
-    if test_list[i] not in test_list[i + 1:]: 
+for i in range(len(test_list)):
+    if test_list[i] not in test_list[i + 1:]:
         res_list.append(test_list[i])
 
 #################################
@@ -126,7 +126,7 @@ with open(file_name, 'w') as outfile:
 # From Kei
 # with open('petal/static/js/hierarchy1.json', 'w') as outfile:
 
-    
+
 file_name = os.path.join( petal_dir, 'static/js', 'hierarchy1.json' )
 # with open('C:/Users/kkojima1/petal/petal/static/js/hierarchy1.json', 'w') as outfile:
 with open(file_name, 'w') as outfile:
