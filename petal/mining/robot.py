6003cf0270ba9a006670f7ea1bd6105a958a7c5b
Lucas Saldyt <lucassaldyt@gmail.com>
2020-01-16 11:12:58 -0700

    Success

--- petal/mining/robot.py
+++ petal/mining/robot.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from requests import get
 from selenium import webdriver
+from time import sleep
 
 from pprint import pprint
 
@@ -9,6 +10,35 @@ import sys, re, os
 year = '2019'
 url = 'https://www.catalogueoflife.org/annual-checklist/{}/browse/tree'.format(year)
 cache_file = 'cached_catalogue_of_life.html'
+
+sleep_time = 1
+
+name_map = ['', 'phylum', 'class', 'order', 'family', 'genus']
+
+def get_parent(node):
+    return node.find_element_by_xpath('../..')
+
+def expand(node):
+    node.click()
+    sleep(sleep_time)
+    parent = get_parent(node)
+    return parent.find_elements_by_class_name('dijitTreeExpandoClosed')
+
+def recursive_expand(node, depth=0):
+    name = get_name(node, depth)
+    # Special cases for first two levels and for last level
+    collected = []
+    children = expand(node)
+    for child in children:
+        found = recursive_expand(child, depth=depth+1)
+        collected.extend([(name,) + f for f in found])
+    return collected
+
+def get_name(node, depth=0, prefix='node-'):
+    parent   = get_parent(node)
+    element  = parent.find_element_by_class_name(prefix + name_map[depth])
+    spanHTML = element.get_attribute('outerHTML')
+    return spanHTML.split('>')[1].split('<')[0]
 
 def main():
     # Load raw HTML of COL (ideally a one-off)
@@ -20,12 +50,21 @@ def main():
     else:
         driver = webdriver.Firefox()
         driver.get(url)
-        expandable = driver.find_elements_by_class_name('dijitTreeExpando')
-        for _ in range(6):
-            for exp in expandable[1:]:
-                if 'Closed' in exp.get_attribute('class'):
-                    exp.click()
-            expandable = driver.find_elements_by_class_name('dijitTreeExpando')
+
+        kingdoms = driver.find_elements_by_class_name('dijitTreeExpandoClosed')
+        for kingdom in kingdoms:
+            k_name = get_name(kingdom, depth=0)
+            phylums = expand(kingdom)
+            for phylum in phylums:
+                p_name = get_name(phylum, depth=1)
+                classes = expand(phylum)
+                for c in classes:
+                    class_species_long_form = recursive_expand(c, depth=2) # Depth is a starting parameter
+                    print(class_species_long_form)
+                phylum.click()
+            kingdom.click()
+
+        return
         html = driver.page_source
         # Cache HTML
         with open(cache_file, 'w') as outfile:
---
+++ petal/mining/setup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+export PATH=/usr/lib/llvm/8/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/bin:/root/projects/PeTaL/petal/gecko/:/root/projects/PeTaL/petal/mining/gecko/:~/projects/PeTaL/petal/mining/gecko/
