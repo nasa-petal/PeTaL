e14bcae4bf20a3d9066047d2c45cc1574b943e55
LSaldyt <lucassaldyt@gmail.com>
2020-01-22 14:34:44 -0500

    Simplifies and re-profiles: minus 3s

--- petal/mining/backbone.py
+++ petal/mining/backbone.py
@@ -10,21 +10,15 @@ from selenium.common.exceptions import ElementNotInteractableException
 
 import sys, re, os
 
-import pickle
-import json
-
 year = '2019'
 url = 'https://www.catalogueoflife.org/annual-checklist/{}/browse/tree'.format(year)
 cache_file = 'cached_catalogue_of_life.html'
 
-sleep_time = 0.01
-
-name_map = ['', 'phylum', 'class', 'order', 'family', 'genus', 'genus']
+sleep_time = 0.005
 
 def load_click(node):
     try:
         node.click()
-        sleep(sleep_time)
     except ElementNotInteractableException:
         sleep(sleep_time)
         load_click(node)
@@ -38,8 +32,7 @@ def get_parent(node):
 def expand(node):
     try:
         load_click(node)
-    except AttributeError as e:
-        print(e)
+    except AttributeError as e: # Can't click top level
         pass
     parent = get_parent(node)
     return parent.find_elements_by_class_name('dijitTreeExpandoClosed')
@@ -48,31 +41,37 @@ def parse_tag(element):
     tag = element.get_attribute('outerHTML')
     return tag.split('>')[1].split('<')[0]
 
-def recursive_expand(node, depth=0):
-    collected = []
+def recursive_expand(node, depth=0, properties=None):
+    if properties is None:
+        properties = dict()
     parent = get_parent(node)
-    name = get_name(node, depth)
+    rank, name = get_name_tuple(node, depth)
+    if rank.strip() == '':
+        rank = 'Kingdom'
+    properties[rank] = name
     children = expand(node)
     # Terminal case
     if len(children) == 0:
         links = parent.find_elements_by_tag_name('a')
         for i in range(len(links) // 2):
             pair = (name, parse_tag(links[i + 1]))
-            collected.append(pair)
+            # TODO: Add to neo4j
     # Normal case
     else:
         for child in children:
             found = recursive_expand(child, depth=depth+1)
-
-            collected.extend([(name,) + f for f in found])
+            if depth < 2:
+                break
     # Collapse to save resources
-    node.click()
-    return collected
+    properties.pop(rank) 
+    if depth > 0:
+        node.click()
 
-def get_name(node, depth=0, prefix='node-'):
-    parent   = get_parent(node)
-    element  = parent.find_element_by_class_name('nodeLabel')#prefix + name_map[depth])
-    return parse_tag(element)
+def get_name_tuple(node, depth=0, prefix='node-'):
+    parent = get_parent(node)
+    name   = parent.find_element_by_class_name('nodeLabel')
+    rank   = parent.find_element_by_class_name('rank')
+    return parse_tag(rank), parse_tag(name)
 
 def main():
     # Load raw HTML of COL (ideally a one-off)
