d271085373fd48337a869932c551216180689301
LSaldyt <lucassaldyt@gmail.com>
2020-01-22 15:14:43 -0500

    Fixes small bugs after testing
    Specifically: Popping at top level, getting rank when not appropriate

--- petal/mining/backbone.py
+++ petal/mining/backbone.py
@@ -7,6 +7,7 @@ from pprint import pprint
 
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import ElementNotInteractableException
+from selenium.common.exceptions import ElementClickInterceptedException
 
 from neo4j import GraphDatabase
 
@@ -24,7 +25,7 @@ cache_file = 'cached_catalogue_of_life.html'
 sleep_time = 0.01
 
 def add_species(tx, properties, pair):
-    catalog_source, name = pair
+    name, catalog_source = pair
     species_info = 'Name: {name},CatalogSource: {catalog_source}'
     taxa_info = ','.join('{key}:{{{key}}}'.format(key=k) for k in properties)
     prop_field = '{' + taxa_info + ', ' +  species_info + '}'
@@ -37,6 +38,9 @@ def load_click(node):
     try:
         node.click()
         sleep(sleep_time)
+    except ElementClickInterceptedException:
+        sleep(sleep_time)
+        load_click(node)
     except ElementNotInteractableException:
         sleep(sleep_time)
         load_click(node)
@@ -62,12 +66,13 @@ def parse_tag(element):
 def recursive_expand(node, depth=0, properties=None, session=None):
     if properties is None:
         properties = dict()
-    parent = get_parent(node)
-    rank, name = get_name_tuple(node, depth)
-    rank = rank.strip()
-    if rank == '':
-        rank = 'Kingdom'
-    properties[rank] = name
+    if depth != 0:
+        parent = get_parent(node)
+        rank, name = get_name_tuple(node, depth)
+        rank = rank.strip()
+        if rank == '':
+            rank = 'Kingdom'
+        properties[rank] = name
     children = expand(node)
     # Terminal case
     if len(children) == 0:
@@ -80,11 +85,9 @@ def recursive_expand(node, depth=0, properties=None, session=None):
     else:
         for child in children:
             found = recursive_expand(child, depth=depth + 1, properties=properties, session=session)
-            if depth < 2:
-                break
     # Collapse to save resources
-    properties.pop(rank) 
     if depth > 0:
+        properties.pop(rank) 
         node.click()
 
 def get_name_tuple(node, depth=0, prefix='node-'):
