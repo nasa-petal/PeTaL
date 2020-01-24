802d83b4ded2458d3cb1b53867fbb959e3032983
LSaldyt <lucassaldyt@gmail.com>
2020-01-24 09:56:52 -0500

    Creates joint neo4j:mongodb prototype

--- petal/mining/driver.py
+++ petal/mining/driver.py
@@ -1,13 +1,46 @@
 from eol  import search as eol_search
 from wiki import search as wiki_search
 
+from neo4j import GraphDatabase
+
+neo_uri = "bolt://localhost:7687"
+neo_client = GraphDatabase.driver(neo_uri, auth=("neo4j", "life"))
+
+from pymongo import MongoClient
+import json
+
+# Connecting to the database
+mongo_db_name = 'test_db'
+username = 'test_user'
+password = 'testing'
+mongo_client = MongoClient('mongodb://{0}:{1}@139.88.179.199:27017/{2}'.format(username,password, mongo_db_name),maxPoolSize=50)
+
+
+def iter_species(tx, mapper):
+    species_result = tx.run('MATCH (n:Species) RETURN n')
+    species = species_result.records()
+    for s in species:
+        mapper(s['n'])
+        break
+
+def count_species(tx):
+    result = tx.run('MATCH (n) WITH COUNT (n) AS count RETURN count')
+    saved = result.single()
+    print('neo4j currently holds ', saved['count'], ' species')
+
+def mapper(species):
+    name = species['Name']
+    print('Mapper on species: ', name)
+    results = eol_search(name)
+    print(results)
+    with mongo_client:
+        eol_db = mongo_client.eol_db
+        eol_db.eol_db.insert_one({name : results})
+
 def main():
-    # TODO: read species from backbone
-
-    species = 'Hapalochlaena lunulata'
-
-    print(eol_search(species))
-    print(wiki_search(species))
+    with neo_client.session() as session:
+        session.read_transaction(count_species)
+        session.read_transaction(iter_species, mapper)
 
 if __name__ == '__main__':
     main()
