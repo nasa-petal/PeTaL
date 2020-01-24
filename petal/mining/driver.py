f70937fc7224ba7d436f7b52f39d125754170ed9
LSaldyt <lucassaldyt@gmail.com>
2020-01-24 14:19:28 -0500

    Removes mongo

--- petal/mining/driver.py
+++ petal/mining/driver.py
@@ -1,20 +1,13 @@
 from eol  import search as eol_search
 from wiki import search as wiki_search
+from scholarly import search_pubs_query as google_scholar_search
 
 from neo4j import GraphDatabase
 
 neo_uri = "bolt://localhost:7687"
 neo_client = GraphDatabase.driver(neo_uri, auth=("neo4j", "life"))
 
-from pymongo import MongoClient
 import json
-
-# Connecting to the database
-mongo_db_name = 'test_db'
-username = 'test_user'
-password = 'testing'
-mongo_client = MongoClient('mongodb://{0}:{1}@139.88.179.199:27017/{2}'.format(username,password, mongo_db_name),maxPoolSize=50)
-
 
 def iter_species(tx, mapper):
     species_result = tx.run('MATCH (n:Species) RETURN n')
@@ -31,11 +24,10 @@ def count_species(tx):
 def mapper(species):
     name = species['Name']
     print('Mapper on species: ', name)
-    results = eol_search(name)
-    print(results)
-    with mongo_client:
-        eol_db = mongo_client.eol_db
-        eol_db.eol_db.insert_one({name : results})
+    scholar_results = google_scholar_search(name + ' source:"journal of experimental biology"')
+    print(next(scholar_results))
+    return
+    # results = eol_search(name)
 
 def main():
     with neo_client.session() as session:
