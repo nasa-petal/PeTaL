9d8779ced2ffec338601b3a8a44c69226ab28da3
LSaldyt <lucassaldyt@gmail.com>
2020-01-30 11:21:23 -0500

    Adds tree example and set similarity example

--- petal/translation/corpora.py
+++ petal/translation/corpora.py
@@ -2,17 +2,19 @@ from nltk.corpus import wordnet as wn
 from pprint      import pprint
 
 import wikipedia
+import pickle, os
 
 from cleaner import Cleaner
 
 class WikipediaTopicNode():
-    def __init__(self, synnode, cleaner):
+    def __init__(self, synnode, cleaner, parent=None):
         print('Creating node for ', synnode, flush=True)
         self.name = (synnode.name().split('.')[0])
         self.children = []
+        self.parent = parent
         try:
             self.page = wikipedia.page(wikipedia.search(self.name)[0], auto_suggest=False)
-            self.summary_text = cleaner.clean(self.page.summary)
+            self.summary_text = list(cleaner.clean(self.page.summary))
         except wikipedia.exceptions.DisambiguationError:
             self.summary_text = ''
 
@@ -28,21 +30,39 @@ class WikipediaTreeScraper():
         pprint(hyponym_tree)
         return self.create_tree(hyponym_tree)
 
-    def create_tree(self, hyponym_tree):
+    def create_tree(self, hyponym_tree, parent=None):
         if len(hyponym_tree) == 1:
             root = hyponym_tree[0]
-            return WikipediaTopicNode(root, self.cleaner)
+            return WikipediaTopicNode(root, self.cleaner, parent=parent)
         root, *children = hyponym_tree
         root = WikipediaTopicNode(root, self.cleaner)
-        root.children = [self.create_tree(subtopic) for subtopic in children]
+        root.children = [self.create_tree(subtopic, parent=root) for subtopic in children]
         return root
 
+def save(data, filename):
+    with open(filename, 'wb') as outfile:
+        pickle.dump(data, outfile)
+
+def load(filename):
+    with open(filename, 'rb') as infile:
+        return pickle.load(infile)
+
+def cache_corpus_tree(name, filename):
+    if not os.path.isfile(filename):
+        tree = scraper.corpus_tree(name)
+        save(tree, filename)
+    else:
+        tree = load(filename)
+    return tree
 
 if __name__ == '__main__':
     bio = 'biology.n.01'
     eng = 'engineering.n.02'
 
-    scraper = WikipediaTreeScraper()
-    print(scraper.corpus_tree(bio))
-    print(scraper.corpus_tree(eng))
+    scraper  = WikipediaTreeScraper()
+    bio_tree = cache_corpus_tree(bio, 'biotree.pkl')
+    eng_tree = cache_corpus_tree(eng, 'engtree.pkl')
+    bio_set  = set((bio_tree.summary_text))
+    eng_set  = set((eng_tree.summary_text))
+    print(set.intersection(bio_set, eng_set))
 
