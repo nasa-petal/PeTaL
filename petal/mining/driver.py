bb65ec760cff626fbb1eb99a8d7d83ec5934d909
LSaldyt <lucassaldyt@gmail.com>
2020-02-03 09:48:39 -0500

    Fixes UUID linking

--- petal/mining/driver.py
+++ petal/mining/driver.py
@@ -11,15 +11,19 @@ from uuid import uuid4
 
 class Driver():
     def __init__(self, page_size=100, rate_limit=0.25):
-        self.neo_client = GraphDatabase.driver("bolt://139.88.179.199:7687", auth=basic_auth("neo4j", "testing"))
+        # self.neo_client = GraphDatabase.driver("bolt://139.88.179.199:7687", auth=basic_auth("neo4j", "testing"))
+        self.neo_client = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "life"))
         self.page_size  = page_size
         self.rate_limit = rate_limit
         self.processes  = dict()
 
     def paging(self, tx, module):
         finder = 'MATCH (n:{}) '.format(module.in_label)
+        print(finder)
         query  = finder + 'RETURN n'
+        print(query)
         for page_results in page(tx, finder, query, page_size=self.page_size, rate_limit=self.rate_limit):
+            print(page_results)
             for record in page_results.records():
                 with self.neo_client.session() as session:
                     node = record['n']
@@ -29,7 +33,7 @@ class Driver():
     def write(self, tx, node, process_result, module):
         if len(process_result) > 0:
             pprint(process_result) # TODO add to db here
-            if module.connect_label is None:
+            if module.connect_labels is None:
                 pass
             else:
                 print('Connect nodes here') # TODO add to db here
@@ -41,16 +45,17 @@ class Driver():
 
     def link(self, tx, id1, id2, module):
         from_label, to_label = module.connect_labels
-        print('MATCH (n:{in_label}) WHERE n.uuid={id1} MATCH (m:{out_label}) WHERE m.uuid={id2} MERGE (n)-[:{from_label}]->(m) MERGE (m)-[:{to_label}]->(n)'.format(in_label=module.in_label, out_label=module.out_label, id1=id1, id2=id2, from_label=from_label, to_label=to_label))
-        1/0
-        # tx.run('MATCH (n: {in_label}) WHERE n.uuid={id1} MATCH (m:out_label) WHERE m.uuid={id2} MERGE (n)-[:{from_label}]->(m) MERGE (m)-[:{to_label}]->(n)', in_label=module.in_label, out_label=module.out_label, id1=id1, id2=id2, from_label=from_label, to_label=to_label)
+        query = ('MATCH (n:{in_label}) WHERE n.uuid=\'{id1}\' MATCH (m:{out_label}) WHERE m.uuid=\'{id2}\' MERGE (n)-[:{from_label}]->(m) MERGE (m)-[:{to_label}]->(n)'.format(in_label=module.in_label, out_label=module.out_label, id1=id1, id2=id2, from_label=from_label, to_label=to_label))
+        print(query)
+        tx.run(query)
+        # tx.run('MATCH (n:{in_label}) WHERE n.uuid={id1} MATCH (m:{out_label}) WHERE m.uuid={id2} MERGE (n)-[:{from_label}]->(m) MERGE (m)-[:{to_label}]->(n)'.format(in_label=module.in_label, out_label=module.out_label), id1=id1, id2=id2, from_label=from_label, to_label=to_label)
 
     def add(self, data, label):
         unique_id = uuid4()
         data['uuid'] = str(unique_id)
         with self.neo_client.session() as session:
             session.write_transaction(add_json_node, label, data)
-        return unique_id
+        return data['uuid']
 
     def run(self, module):
         if module.in_label is None:
@@ -61,7 +66,7 @@ class Driver():
                 session.read_transaction(self.paging, module)
 
 if __name__ == '__main__':
-    driver = Driver(page_size=10, rate_limit=0.25)
+    driver = Driver(page_size=1, rate_limit=0.25)
     wiki_scraper = WikipediaModule()
     # backbone = BackboneModule()
     # driver.run(backbone)
