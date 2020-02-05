6449c5c514b66c70bcc60ffbf8e67fc793430f25
LSaldyt <lucassaldyt@gmail.com>
2020-02-05 16:23:22 -0500

    Simplifies templates and queries

--- petal-django/bird-demo/views.py
+++ petal-django/bird-demo/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context = dict(title='Home')
+    context = dict(query='')
     return render(request, 'bird/bird_e2b.html', context)
 
 def results(request):
-    context = dict(title='Results')
+    query = request.GET.get('q')
+    # GET RESULTS(query)
+    context = dict(query=query)
     return render(request, 'bird/bird_e2b.html', context)
