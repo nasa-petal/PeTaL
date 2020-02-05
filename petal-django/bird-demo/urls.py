6a8c9cdc5092674b9db28e891f8aeb0b4df7799d
LSaldyt <lucassaldyt@gmail.com>
2020-02-05 15:27:49 -0500

    Fixes/improves search bar

--- petal-django/bird-demo/urls.py
+++ petal-django/bird-demo/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('bird-results/', views.results, name='bird-results'),
     path('', views.index, name='index'),
 ]
--- petal-django/bird-demo/views.py
+++ petal-django/bird-demo/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def index(request):
     context = dict(title='Home')
     return render(request, 'bird/bird_e2b.html', context)
+
+def results(request):
+    context = dict(title='Results')
+    return render(request, 'bird/bird_e2b.html', context)
