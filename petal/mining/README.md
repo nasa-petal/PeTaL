951e7c8dba1c0a837108d419fb25ff3d3982f8d5
LSaldyt <lucassaldyt@gmail.com>
2020-02-11 14:23:52 -0500

    Simplifies driver interface by introducing Transaction class

--- petal/mining/README.md
+++ petal/mining/README.md
@@ -1,1 +1,3 @@
-gecko/geckodriver is an executable less than 6Mb which runs firefox. There might be a better solution than including this directly.
+# Data Mining
+
+
--- petal/mining/driver.py
+++ petal/mining/driver.py
@@ -17,30 +17,17 @@ class Driver():
         # self.neo_client = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "life"))
         self.tracker = None
 
-    def write(self, tx, node, process_result, module):
-        # Future: Simplify this interface? Currently allows [dict], [str], [tuple], dict, str, tuple or None from module.process()
-        # TBH the flexibility of this interface makes things more modular
-        if not isinstance(process_result, list):
-            process_result = [process_result]
-        for result in process_result:
-            if result is None:
+    def write(self, tx, node, transactions, module):
+        for transaction in transactions:
+            if transaction is None:
                 pass
-            elif isinstance(result, str): # Result is a query
-                tx.run(result)
+            if transaction.query is not None:
+                tx.run(transaction.query)
             else:
-                if isinstance(result, dict):
-                    in_label  = module.in_label
-                    out_label = module.out_label
-                    connect_labels = module.connect_labels
-                    result = result
-                elif isinstance(result, tuple):
-                    in_label, out_label, connect_labels, result = result
-                else:
-                    raise ValueError('Invalid scraping module process(node) result: {}'.format(result))
                 id1 = node['uuid']
-                id2 = self.add(result, out_label)
+                id2 = self.add(transaction.data, transaction.out_label)
                 if connect_labels is not None:
-                    self.link(tx, id1, id2, in_label, out_label, *connect_labels)
+                    self.link(tx, id1, id2, transaction.in_label, transaction.out_label, *transaction.connect_labels)
 
     def link(self, tx, id1, id2, in_label, out_label, from_label, to_label):
         query = ('MATCH (n:{in_label}) WHERE n.uuid=\'{id1}\' MATCH (m:{out_label}) WHERE m.uuid=\'{id2}\' MERGE (n)-[:{from_label}]->(m) MERGE (m)-[:{to_label}]->(n)'.format(in_label=in_label, out_label=out_label, id1=id1, id2=id2, from_label=from_label, to_label=to_label))
@@ -69,8 +56,8 @@ class Driver():
         for record in node.records():
             with self.neo_client.session() as session:
                 node = record['n']
-                result = module.process(node)
-                session.write_transaction(self.write, node, result, module)
+                transactions = module.process(node)
+                session.write_transaction(self.write, node, transactions, module)
 
     def run_id(self, module, tracker, node_id):
         self.tracker = tracker
@@ -80,7 +67,7 @@ class Driver():
     def run(self, module, tracker, info):
         self.tracker = tracker
         i = 0
-        for node_json in module.process():
+        for transaction in module.process():
             info.set_current(i)
-            self.add(node_json, module.out_label)
+            self.add(transaction.data, transaction.out_label)
             i += 1
