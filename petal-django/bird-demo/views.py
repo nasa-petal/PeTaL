548087c5d180309b0833de25a326e7d1ee5e478c
LSaldyt <lucassaldyt@gmail.com>
2020-02-11 13:56:30 -0500

    Updates

--- petal-django/bird-demo/views.py
+++ petal-django/bird-demo/views.py
@@ -9,10 +9,11 @@ def index(request):
 
 def results(request):
     query = request.GET.get('q')
-    neo_client = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "life"))
+    # neo_client = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "life"))
+    neo_client = GraphDatabase.driver("bolt://139.88.179.199:7667", auth=basic_auth("neo4j", "testing"))
     with neo_client.session() as session:
         result = session.run('MATCH (a:Article) WHERE a.abstract CONTAINS \'{test}\' RETURN a'.format(test=query))
     articles = [article['a'] for article in result.records()]
     # context = dict(query=result, papers=[dict(url='URL', relevancy=0.0, title='An example', abstract='Lorem ipsum imet ')])
     context = dict(papers=articles)
-    return render(request, 'bird/bird_results.html', context)
+    return render(request, 'bird_results.html', context)
