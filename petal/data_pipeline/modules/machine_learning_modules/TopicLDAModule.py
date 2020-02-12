18d1b7c74345786aa218e4f2f0cd38f3af0a64a7
LSaldyt <lucassaldyt@gmail.com>
2020-02-12 13:52:41 -0500

    Adds online learner and TopicLDA Learner

--- petal/data_pipeline/modules/machine_learning_modules/TopicLDAModule.py
+++ petal/data_pipeline/modules/machine_learning_modules/TopicLDAModule.py
@@ -1,12 +1,18 @@
-from ..module_utils.module import Module
-from ..libraries.topic_modelling import TopicModeler
+from ..machine_learning_utils.OnlineLearner import OnlineLearner
+from ..libraries.topic_modelling            import TopicModeler
 
-class TopicLDAModule(Module):
+import pickle
+
+class TopicLDAModule(OnlineLearner):
     '''
     Model topics of articles in online batches
     '''
-    def __init__(self, in_label='Article', out_label=None, connect_labels=None, name='TopicLDAModule'):
-        Module.__init__(self, in_label, out_label, connect_labels, name)
+    def __init__(self, filename='data/models/topic_model.pkl'):
+        OnlineLearner.__init__(self, in_label='Article', name='TopicLDAModule', filename=filename)
 
-    def process(self, node):
-        print(node)
+    def init_model(self):
+        self.model = TopicModeler()
+    
+    def learn(self, model, batch):
+        print(batch)
+        self.model.update([batch['abstract']])
--- petal/data_pipeline/modules/mining_modules/WikipediaModule.py
+++ petal/data_pipeline/modules/mining_modules/WikipediaModule.py
@@ -20,23 +20,20 @@ class WikipediaModule(Module):
     def process(self, node):
         # Lookup the species based on its name. Make sure that all Species objects have this attribute!!
         name = node['name']
-        try:
-            results = wikipedia.search(name) # Use Wikipedia API to get list of results from searching the species name, throttled to 10 results by default
-            properties = list()
-            for result in results: # Create a transaction for each result
-                result_properties = dict() # Properties for the neo4j node, populated below
-                try:
-                    page = wikipedia.page(result, auto_suggest=True, redirect=True, preload=True) # Use wikiAPI to load the actual page
-                    for field in SCRAPE_FIELDS: # Store only the desired properties (above) in the node properties
-                        try:
-                            result_properties[field] = getattr(page, field)
-                        except KeyError:
-                            pass
-                except wikipedia.exceptions.WikipediaException as e:
-                    pass
-                properties.append(self.default_transaction(result_properties)) # Only create default transaction objects
-                # In the future, use self.custom_transaction() and self.query_transaction() for more complicated Data Mining Modules
-            return properties
-        except Exception as e:
-            return None
+        properties = list()
+        results = wikipedia.search(name)
+        for result in results: # Create a transaction for each result
+            result_properties = dict() # Properties for the neo4j node, populated below
+            try:
+                page = wikipedia.page(result, auto_suggest=True, redirect=True, preload=True) # Use wikiAPI to load the actual page
+                for field in SCRAPE_FIELDS: # Store only the desired properties (above) in the node properties
+                    try:
+                        result_properties[field] = getattr(page, field)
+                    except KeyError:
+                        pass
+            except wikipedia.exceptions.WikipediaException as e:
+                pass
+            properties.append(self.default_transaction(result_properties)) # Only create default transaction objects
+        # In the future, use self.custom_transaction() and self.query_transaction() for more complicated Data Mining Modules
+        return properties
 
