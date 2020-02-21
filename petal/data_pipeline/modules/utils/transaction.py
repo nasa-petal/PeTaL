54177a5b9c2dff5c95ae6b99013fa8a30f6b02fa
LSaldyt <lucassaldyt@gmail.com>
2020-02-21 15:39:56 -0500

    Adds extra bug fixes to UUID isssues

--- petal/data_pipeline/modules/utils/transaction.py
+++ petal/data_pipeline/modules/utils/transaction.py
@@ -1,10 +1,12 @@
-from uuid import uuid4
+# from uuid import uuid4
+import hashlib
 
 class Transaction:
     def __init__(self, in_label=None, out_label=None, connect_labels=None, data=None, query=None, uuid=None, from_uuid=None):
         if uuid is None:
-            print('Had to generate new UUID', flush=True)
-            uuid = str(uuid4())
+            if data is not None:
+                print('Had to generate new UUID', flush=True)
+                uuid = str(hashlib.md5(','.join(map(str, sorted(data.items())))).encode('utf-8').hexdigest())
         self.in_label       = in_label
         self.out_label      = out_label
         self.connect_labels = connect_labels
