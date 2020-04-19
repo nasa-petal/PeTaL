8b9cb91fa7f21564aeee564db2791ec479b75009
LSaldyt <lucassaldyt@gmail.com>
2020-04-19 11:43:10 -0400

    Removes more top-level files

--- setup.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup
-
-setup(name = 'PeTaL',
-      description = 'Periodic Table of Life',
-      packages = ['flask', 'ontospy', 'networkx', 'rdflib', 'pandas', 'Keras', 'numpy', 'Pillow', 'sqlalchemy', 'Flask-SQLAlchemy', 'tensorflow'])
