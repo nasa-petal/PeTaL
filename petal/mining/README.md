5a582417c077bab8d70383dc60f9db170aa7ce22
LSaldyt <lucassaldyt@gmail.com>
2020-04-19 12:19:21 -0400

    Removes old PeTaL.

--- petal/mining/README.md
+++
@@ -1,1 +0,0 @@
-gecko/geckodriver is an executable less than 6Mb which runs firefox. There might be a better solution than including this directly.
--- petal/mining/modules/__init__.py
+++
@@ -1,4 +0,0 @@
-from .google_scholar import GoogleScholarModule
-from .wikipedia      import WikipediaModule
-from .backbone       import BackboneModule
-from .eol            import EOLModule
