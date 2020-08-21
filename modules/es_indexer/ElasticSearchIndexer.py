96da6b1c9ce4c60d35d6a3a0fe8eeeb6f222c155
keikojima02 <kei.kojima02@gmail.com>
2020-08-21 14:05:32 -0400

    Update ElasticSearchIndexer.py

--- modules/es_indexer/ElasticSearchIndexer.py
+++ modules/es_indexer/ElasticSearchIndexer.py
@@ -1,29 +1,9 @@
 from bitflow.utils.module import Module
-
-# Import Elasticsearch package 
 from elasticsearch import Elasticsearch
-
-
-# def create_index(client):
-#     """Creates an index in Elasticsearch if one isn't already there."""
-#     client.indices.create(
-#         index="articles",
-#         body={
-#             "settings": {"number_of_shards": 1},
-#             "mappings": {
-#                 "properties": {
-#                     "title": {"type": "text"},
-#                     "abstract": {"type": "text"},
-#                 }
-#             },
-#         },
-#         ignore=400,
-#     )
-
 
 class ElasticSearchIndexer(Module):
     '''
-    Creates an index in Elastic Search and adds articles as documents
+    A simple placeholder indexer for Elastic search
     '''
     def __init__(self, in_label='Article', name='ElasticSearchIndexer'):
         Module.__init__(self, in_label, name, page_batches=True)
@@ -34,11 +14,8 @@ class ElasticSearchIndexer(Module):
         '''
 
         # Connect to the elastic cluster
-        #client = Elasticsearch('http://localhost:9200')
         client=Elasticsearch([{'host':'localhost','port':9200}])
 
-        print("Creating an index if it one doesnt exist already...")
-        # create_index(client)
 
         body = {
             "title":previous.data["title"],
