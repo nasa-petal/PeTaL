e8e27bf3ac9c5d4f3778d31b6e2ece0f4bb8ee34
Brandon Ruffridge <bruffridge@nasa.gov>
2020-09-25 12:26:06 -0400

    added some example UI elements.

--- src/App.js
+++ src/App.js
@@ -2,32 +2,32 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
-import ProTip from './ProTip';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 export default function App() {
   return (
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v4-beta example
+          How does nature...
         </Typography>
-        <ProTip />
-        <Copyright />
+        <Autocomplete
+          id="function"
+          options={functionList}
+          getOptionLabel={(option) => option.label}
+          style={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="" variant="outlined" />}
+        />
       </Box>
     </Container>
   );
 }
+
+const functionList = [
+  { label: 'Reduce drag', id: 1 },
+  { label: 'Absorb shock', id: 2 },
+  { label: 'Dissipate heat', id: 3 },
+  { label: 'Increase lift', id: 4 },
+  { label: 'Remove particles from a surface', id: 5 }
+]
