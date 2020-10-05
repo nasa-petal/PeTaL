589f24b50aeddf52921210e4e0793e0e074a238f
Brandon Ruffridge <bruffridge@nasa.gov>
2020-10-05 14:28:37 -0400

    removed unnused code.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, {Component} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
-import lizard from './static/images/contemplative-reptile.jpg';
 
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
