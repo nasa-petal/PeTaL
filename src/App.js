92cdabd6362eda6b50669e1a82ee6f971918c566
Brandon Ruffridge <bruffridge@nasa.gov>
2020-10-05 13:44:08 -0400

    added image and wikipedia link to results

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
 import lizard from './static/images/contemplative-reptile.jpg';
 
 import Container from '@material-ui/core/Container';
@@ -27,22 +28,22 @@ function MediaCard(props) {
     <Card className={classes.root}>
       <CardMedia
         className={classes.media}
-        image={lizard}
+        image={props.article.image}
         title={props.article.title}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="h2">
-          {props.article.title}
+          <Link
+            color="primary"
+            href={props.article.url}
+          >
+            {props.article.title}
+          </Link>
         </Typography>
         <Typography variant="body2" color="textSecondary" component="p">
           {props.article.summary}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
     </Card>
   );
 }
