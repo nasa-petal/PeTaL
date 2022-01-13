f76eca0d23293d55b99bd0aa9f95b2b9c7909a41
Brandon Ruffridge <bruffridge@users.noreply.github.com>
2022-01-12 20:26:33 -0500

    replaced temporary MAG requests with API calls to petal api.

--- src/App.js
+++ src/App.js
@@ -35,16 +35,10 @@ function MediaCard(props) {
             color="primary"
             target="_blank"
             rel="noopener noreferrer"
-            href={props.article.S ? props.article.S[0].U : ''}
+            href={props.article.url.S}
           >
-            {props.article.DN}
+            {props.article.title.S}
           </Link>
-        </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-          MAG topics:
-        {props.article.F.map((topic, index) => (
-          <span>{ " " + topic.FN + ',' }</span>
-        ))}
         </Typography>
       </CardContent>
     </Card>
@@ -77,71 +71,49 @@ class App extends Component {
         return;
       }
 
-      // Query MAG
-
-      
-      switch(this.state.selection.id) {
-        case 'modify/convert_thermal_energy':
-          query = "Or(Composite(F.FN=='thermal converter'),Composite(F.FN=='thermal control'),Composite(F.FN=='thermal balance'))";
-          break;
-        case 'distribute_energy':
-          query = "Or(Composite(F.FN=='thermal distribution'),Composite(F.FN=='heat transfer process'),Composite(F.FN=='transient heat transfer'),Composite(F.FN=='heat spreading'),Composite(F.FN=='heat flow'),Composite(F.FN=='heat transfer fluid'),Composite(F.FN=='thermal emission'),Composite(F.FN=='thermal transport'),Composite(F.FN=='bioheat transfer'),Composite(F.FN=='heat transfer model'),Composite(F.FN=='heat spreading'))";
-          break;
-        case 'protect_from_temperature':
-          query = "Or(Composite(F.FN=='thermal resistance'),Composite(F.FN=='passive cooling'),Composite(F.FN=='thermal control'),Composite(F.FN=='thermal fatigue'),Composite(F.FN=='thermal strain'),Composite(F.FN=='thermal dissipation'),Composite(F.FN=='convective cooling'),Composite(F.FN=='thermal buckling'),Composite(F.FN=='thermal residual stress'),Composite(F.FN=='thermal degradation of polymers'),Composite(F.FN=='space shuttle thermal protection system'),Composite(F.FN=='heat spreading'),Composite(F.FN=='heat tolerance'),Composite(F.FN=='thermoregulation'))";
-          // ,Composite(F.FN=='temperature stress'),Composite(F.FN=='heat stress')
-          break;
-        case 'sense_temperature_cues':
-          query = "Or(Composite(F.FN=='thermal sensing'),Composite(F.FN=='temperature measurement'),Composite(F.FN=='temperature monitoring'),Composite(F.FN=='temperature sensing'),Composite(F.FN=='thermal detector'),Composite(F.FN=='thermal monitoring'),Composite(F.FN=='thermal probe'),Composite(F.FN=='thermal sensors'))";
-          break;
-        case 'store_energy':
-          query = "Composite(F.FN=='solar thermal collector')";
-          break;
-      }
-
-      var query = "And(Ty='0'," + query + ",Or(Composite(J.JN=='biomimetics'), Composite(F.FN=='biology')))";
-
-      const url = new URL('https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate');
-      const params = {
-        expr: query,
-        model: 'latest',
-        count: 20000,
-        offset: 0,
-        attributes: 'Id,DOI,DN,VFN,F.FN,AA.AuId,AW,RId,S'
-      }
-      url.search = new URLSearchParams(params).toString();
-
-      fetch(url, {
-        headers: {
-          'Ocp-Apim-Subscription-Key': 'd969a6c4bef34765ae7c5f0e75dd624e'
-        }
-      })
-        .then(res => res.json())
-        .then((data) => {
-          this.setState({ fetchInProgress: false });
-          this.setState({ articlesToDisplay: data.entities })
-        })
-        .catch(console.log)
-
       //querying the database by selected label
-      /*const selection_label = this.state.selection.id
-      const url = new URL('https://unbqm6hov8.execute-api.us-east-2.amazonaws.com/v1/getarticles')
+      const getAllData = async (params) => {
+        const _getAllData = async (params, startKey) => {
+          if (startKey) {
+            params.sortkey = startKey.SortKey.S
+            params.partkey = startKey.PartitionKey.S
+          }
+          // assigning label to url
+          url.search = new URLSearchParams(params).toString()
+
+          return fetch(url).then(res => res.json())
+        }
+        let lastEvaluatedKey = null
+        let rows = []
+        do {
+          const result = await _getAllData(params, lastEvaluatedKey)
+          rows = rows.concat(result.Items)
+          lastEvaluatedKey = result.LastEvaluatedKey
+        } while (lastEvaluatedKey)
+        return rows
+      }
+
+      const selection_label = this.state.selection.id
+      const url = new URL('https://ardwrgr7s5.execute-api.us-east-2.amazonaws.com/v1/getarticles')
       const params = { level3: selection_label }
-      // assigning label to url
-      url.search = new URLSearchParams(params).toString();
-      fetch(url)
-        .then(res => res.json())
-        .then((data) => {
-          this.setState({ articlesToDisplay: data.Items })
-        })
-        .catch(console.log)*/
+
+      getAllData(params).then((data) => {
+        this.setState({ fetchInProgress: false });
+
+        // sort papers by scores DESC.
+        data.sort(function (a, b) {
+          return parseFloat(b.score.S) - parseFloat(a.score.S);
+        });
+
+        this.setState({ articlesToDisplay: data })
+      }).catch(console.log)
     });
   }
 
   render() {
 
     const articleCards = this.state.articlesToDisplay.map((article) =>
-      <Grid item xs={12} key={article.Id}><MediaCard article={article} /></Grid>
+      <Grid item xs={12} key={article.SortKey.S}><MediaCard article={article} /></Grid>
     );
 
     return (
@@ -183,7 +155,7 @@ class App extends Component {
 
   componentDidMount() {
     // connect to petal-api to fetch articles list.
-    fetch('https://unbqm6hov8.execute-api.us-east-2.amazonaws.com/v1/getalllabels')
+    fetch('https://ardwrgr7s5.execute-api.us-east-2.amazonaws.com/v1/getalllabels')
     .then(res => res.json())
     .then((data) => {
 
@@ -191,13 +163,11 @@ class App extends Component {
       let labels = data.Items;
 
       labels.forEach(label => {
-        if(['Modify/convert thermal energy', 'Sense temperature cues', 'Protect from temperature', 'Store energy', 'Distribute energy'].includes(label.Level3.S)){
-          functions.push({
-            id: label.Level3.S.toLowerCase().split(' ').join('_'),
-            level2: label.Level2.S,
-            level3: label.Level3.S
-          })
-        }
+        functions.push({
+          id: label.level3.S.toLowerCase().split(' ').join('_'),
+          level2: label.level2.S,
+          level3: label.level3.S
+        })
       })
 
       this.setState({ functions: functions })
